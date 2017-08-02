bd3d65c84a67cd4630a8c8ac5905899047ec9155
edc <endou_cai@sinobasedm.com>
2017-08-02 15:15:47 +0800

    page member

--- memberSystem/app/views.py
+++ memberSystem/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from models import Member
 
 # Create your views here.
+def member(request):
+    members = Member.objects.all()
+    return render(request, 'index.html', {'members':members})
+
