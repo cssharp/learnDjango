33128b872f6ea4cfc3489ffc61af5aab90a5cf61
edc <endou_cai@sinobasedm.com>
2017-08-02 15:10:16 +0800

    +TEMPLATES

--- memberSystem/memberSystem/settings.py
+++ memberSystem/memberSystem/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'memberSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
