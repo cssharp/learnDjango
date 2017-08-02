442823978493bf0d37f7658d1d91d44de451232d
edc <endou_cai@sinobasedm.com>
2017-08-02 12:04:27 +0800

    null=True

--- memberSystem/app/models.py
+++ memberSystem/app/models.py
@@ -7,14 +7,14 @@ class Member(models.Model):
         verbose_name = '用户' #提供了一个更容易让人阅读的名称
         verbose_name_plural = '用户' #复数形式
     memberId = models.AutoField(primary_key=True) #models.IntegerField(auto_created=True)
-    email = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, null=True)
     userName = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    score = models.IntegerField()
+    password = models.CharField(max_length=50, null=True)
+    score = models.IntegerField(null=True)
     isDeleted = models.BooleanField()
     isEnabled = models.BooleanField()
     createTime = models.DateTimeField()
-    myCode = models.CharField(max_length=50)
+    myCode = models.CharField(max_length=50, null=True)
 
     def __unicode__(self):
         return self.userName
