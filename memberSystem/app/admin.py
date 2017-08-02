40acf1236ef977351687afd76d81c67e57c9bba1
edc <endou_cai@sinobasedm.com>
2017-08-02 17:59:16 +0800

    管理列表样式：显示列+搜索列

--- memberSystem/app/admin.py
+++ memberSystem/app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from models import Member, Order
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('mobile', 'userName', 'createTime', 'isReceiveOrder')
+    search_fields = ('mobile', 'userName')
+
 # Register your models here.
 admin.site.register(Member)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
+
