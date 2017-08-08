35cce7554c9e1931f8922267d62b7fc4285ed5c0
edc <endou_cai@sinobasedm.com>
2017-08-08 12:03:58 +0800

    set media root & media url

--- memberSystem/memberSystem/settings.py
+++ memberSystem/memberSystem/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = '/home/edc/project/learnDjango/memberSystem/app/static/media/'
+MEDIA_URL = '/static/media/'
